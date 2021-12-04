9954b9ec9db4e630f664e8189887c3082254cb2a
Interfiber <webmaster@interfiber.dev>
2021-12-04 18:04:47 -0500

    :sparkles: Save changes on build

--- pakr/src/editor.rs
+++ pakr/src/editor.rs
@@ -1,5 +1,7 @@
-use std::path::Path;
+use crate::project;
+use crate::pak_cli;
 use iced::button;
+use serde_json::json;
 use iced::{Button, Column, Text, Sandbox, Settings, Element, Align};
 use hashbrown::HashMap;
 use iced::text_input::TextInput;
@@ -12,6 +14,7 @@ pub struct Editor {
     build_project_button: button::State,
     load_project_button: button::State,
     project_info: HashMap<String, String>,
+    components: HashMap<String, Value>,
     project_info_name: String,
     project_info_orgid: String
 }
@@ -31,7 +34,38 @@ impl Sandbox for Editor {
     fn update(&mut self, message: Message) {
         match message {
            Message::BuildProject => {
-               println!("Building project...");
+                println!("Building project...");
+                let project_dir = project::get_current_project();
+                println!("Switching to working directory: {}", project_dir);
+                match std::env::set_current_dir(project_dir.to_string()){
+                    Ok(_) => print!(""),
+                    Err(err) => {
+                        println!("Failed to change current working directory!");
+                        println!("Error Log: {}", err);
+                        std::process::exit(1);
+                    }
+                }
+                println!("Saving config to disk...");
+                // read the config off disk
+                let project_raw = std::fs::read_to_string(&format!("{}/pak.project.json", project_dir.to_string())).expect("Failed to read project config off disk!");
+                let mut project_config: Value = serde_json::from_str(&project_raw).unwrap();
+                let config_obj = project_config.as_object_mut().unwrap();
+                let project_name = self.project_info.get("projectName").expect("Failed to find data: projectName not in file").to_string();
+                let org_id =  self.project_info.get("orgId").expect("Failed to find data: orgId not in file").to_string();
+                config_obj.insert("projectName".to_string(), Value::String(project_name));
+                config_obj.insert("orgName".to_string(), Value::String(org_id));
+                match std::fs::write(&format!("{}/pak.project.json", project_dir.to_string()), serde_json::to_string_pretty(&config_obj).unwrap()){
+                    Ok(_) => print!(""),
+                    Err(err) => {
+                        println!("Failed to write project file!");
+                        println!("Error: {}", err);
+                        std::process::exit(1);
+                    }
+                }
+                // update it for the new config
+                // write it to disk
+                println!("Executing build command");
+                pak_cli::execute_pak_cmd("build");
            },
             Message::ProjectNameUpdated(name) => {
                 println!("Updating project name to {}...", name);
@@ -45,28 +79,16 @@ impl Sandbox for Editor {
             },
             Message::LoadProjectInfo => {
                 println!("Loading project from on-disk files...");
-                let project_tmp_file = "/tmp/pakr.project_path";
-                // read from file
-                println!("Reading from: {}", project_tmp_file);
-                let project_path = std::fs::read_to_string(project_tmp_file).expect("Failed to read contents of pakr project temp file");
-                println!("File path: {}", project_path);
-                //     Check if the file exists
-                let project_exists = Path::new(&project_path).exists();
-                let project_file_exists = Path::new(&format!("{}/pak.project.json", project_path)).exists();
-                if !project_exists {
-                    println!("Failed to load project: No such file or directory {}", project_path);
-                    std::process::exit(1);
-                }
-                if !project_file_exists {
-                    println!("Failed to load project: Failed to find project file in the project directory!");
-                    std::process::exit(1);
-                }
+                let project_path = project::get_current_project();
                 let project_raw_data = std::fs::read_to_string(&format!("{}/pak.project.json", project_path)).expect("Failed to open project file!");
-                //          Parse project config
+                //  Parse project config
                 let project_config: Value = serde_json::from_str(&project_raw_data).expect("Failed to parse project data");
                 println!("{:?}", project_config);
                 self.project_info_name = project_config["projectName"].to_string().replace("\"", "");
                 self.project_info_orgid = project_config["orgName"].to_string().replace("\"", "");
+                self.project_info.insert("orgId".to_string(), project_config["orgName"].to_string().replace("\"", ""));
+                self.project_info.insert("projectName".to_string(), project_config["projectName"].to_string().replace("\"", ""));
+                println!("Loaded project info");
             }
         }
     }
@@ -93,7 +115,7 @@ impl Sandbox for Editor {
                 Text::new("\n").height(iced::Length::Units(10))
             )
             .push(
-                Button::new(&mut self.build_project_button, Text::new("Build Project")).on_press(Message::BuildProject)
+                Button::new(&mut self.build_project_button, Text::new("Build/Save Project")).on_press(Message::BuildProject)
             )
             .push(
                 Text::new("\n").height(iced::Length::Units(15))
--- pakr/src/project.rs
+++ pakr/src/project.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::env;
 use std::process::Command;
+use std::path::Path;
 pub fn create_project(folder: String) -> bool{
     println!("Creating project in: {}", folder);
     match fs::create_dir_all(folder.to_string()){
@@ -32,4 +33,20 @@ pub fn open_project(folder: String){
     let _cmd = Command::new(current_exe).arg(folder).spawn();
     println!("Exiting main window...");
     std::process::exit(0);
+}
+pub fn get_current_project() -> String {
+    let project_tmp_file = "/tmp/pakr.project_path";
+    let project_path = std::fs::read_to_string(project_tmp_file).expect("Failed to read contents of pakr project temp file");
+    //     Check if the file exists
+    let project_exists = Path::new(&project_path).exists();
+    let project_file_exists = Path::new(&format!("{}/pak.project.json", project_path)).exists();
+    if !project_exists {
+        println!("Failed to load project: No such file or directory {}", project_path);
+        std::process::exit(1);
+    }
+    if !project_file_exists {
+        println!("Failed to load project: Failed to find project file in the project directory!");
+        std::process::exit(1);
+    }
+    return project_path;
 }
